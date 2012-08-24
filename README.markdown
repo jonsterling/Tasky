e25ae1e89798a490e1841f9e65f94fee9ed872c7
Jonathan Sterling <jonsterling@me.com>
2012-08-24 14:10:27 -0700

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,72 @@
+# Taskwarrior Utilities
+
+Just put these files in your path! To make a `tasky` command, do the
+following:
+
+    ln -s tasky.py tasky
+    chmod +x tasky
+
+## Tasky: a modal, interactive Taskwarrior client
+
+Tasky is pretty well undocumented, but it is basically a curses-based
+shell to make interacting with Taskwarrior easier. The main keys are the
+following:
+
+1. `j`, `k`: scrolling.
+
+2. `i`: insert new task.
+
+3. `e` or `RETURN`: edit the selected task (see below).
+
+4. `c`: complete the selected task. No confirmation.
+
+5. `d`: delete the selected task. No confirmation.
+
+6. `u`: undo whatever you did last. No confirmation.
+
+7. `:`: enter **command mode**. Basically, enter any arbitrary
+   Taskwarrior commands (without `task`). Example: `proj:tasky del`.
+
+8. `!`: enter **shell mode**. Run any arbitrary shell command.
+
+9. `f`: change the current filter (see below).
+
+10. `n`: runs `tasknote` for a given task in a new `tmux` pane. Since I
+    built Tasky for myself, this command requires you to be in a `tmux`
+    session. I plan to make this configurable later.
+
+Tasky supports mouse input! You can click a task to select it, and
+scrolling also works.
+
+
+### Filters
+
+Filters in Tasky are quite simple. In order to get your tasks, Tasky
+runs `task export`; a *filter* is simply a string that is appended to
+the `task export` command. So, a valid filter would be something like
+`proj:tasky` or `due:today` or `due:tomorrow +important`. The current
+filter is displayed in the header.
+
+To clear the filter, simply type `f` and hit return without entering any
+text.
+
+### Editing and Inserting
+
+The text you enter is appended to a `task add` or `task # mod` command.
+That means you can change a task's project or tags or due date using
+this mode.
+
+When adding new tasks, the current filter is appended. That means that
+if you are currently viewing tasks in a certain project that are due
+today, new tasks will also have those attributes.
+
+
+### Still to do
+
+Tasky does not yet support sorting or arbitrary searching.
+
+
+## Tasknote
+
+Tasknote is another small utility that allows you to associate a text
+file with a task. It is included in this repository.
---
+++ tasky.py
@@ -0,0 +1,274 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+import urwid
+import json
+import subprocess
+import datetime
+import sys
+
+class TaskWarrior(object):
+
+    def pending_tasks(self, args=''):
+        raw_output = subprocess.check_output(['task', 'export',
+            'status:pending', args])
+
+        task_json = '[%s]' % raw_output
+        return json.loads(task_json, strict=False)
+
+    def complete(self, task):
+        subprocess.call(['task', task['uuid'], 'done'])
+
+    def delete(self, task):
+        subprocess.call(['task', task['uuid'], 'rc.confirmation:no', 'del'])
+
+    def add(self, value):
+        subprocess.call(['task', 'add', value])
+
+    def mod(self, task, value):
+        subprocess.call(['task', task['uuid'], 'mod', value])
+
+    def undo(self):
+        subprocess.call(['task', 'rc.confirmation:no', 'undo'])
+
+
+class TaskWidget (urwid.WidgetWrap):
+
+    def __init__ (self, task):
+        self.task = task
+
+        desc = urwid.Text(task.get('description', ''))
+        proj = urwid.Text(task.get('project','') + ' ', align='right')
+
+        due = urwid.Text(self._due_date_string() + ' ', align='right')
+
+        item = urwid.AttrMap(urwid.Columns([
+            ('fixed', 11, urwid.AttrWrap(proj, 'proj', 'proj_focus')),
+            desc,
+            ('fixed', 11, due)
+        ]), 'body', 'body_focus')
+
+        urwid.WidgetWrap.__init__(self, item)
+
+    def selectable (self):
+        return True
+
+    def keypress(self, size, key):
+        return key
+
+    def _due_today(self):
+        return self._due_date() == datetime.datetime.today().date()
+
+    def _due_date(self):
+        if 'due' in self.task:
+            return datetime.datetime.strptime(self.task['due'][:8],
+                    "%Y%m%d").date()
+        return None
+
+    def _due_date_string(self):
+        if self._due_today():
+            return 'today'
+
+        date = self._due_date()
+        if date:
+            return date.strftime("%m/%d")
+
+        return ''
+
+
+
+class LineEditor(urwid.Edit):
+
+    __metaclass__ = urwid.signals.MetaSignals
+    signals = ['done']
+
+    def keypress(self, size, key):
+        if key == 'enter':
+            urwid.emit_signal(self, 'done', self.get_edit_text())
+            return
+
+        if key == 'esc':
+            urwid.emit_signal(self, 'done', None)
+            return
+
+        return urwid.Edit.keypress(self, size, key)
+
+
+class ScrollingListBox(urwid.ListBox):
+
+    def scroll_down(self):
+        focus = self.get_focus()[1]
+        if focus is not None:
+            self.set_focus(self.get_focus()[1] + 1)
+
+    def scroll_up(self):
+        focus = self.get_focus()[1]
+        if focus > 0:
+            self.set_focus(focus - 1)
+
+    def mouse_event(self, size, event, button, col, row, focus):
+        button_map = {
+            4: self.scroll_down,
+            5: self.scroll_up
+        }
+
+        if button in button_map:
+            button_map[button]()
+
+        return urwid.ListBox.mouse_event(self, size, event, button, col, row, focus)
+
+    def keypress(self, size, key):
+        key_map = {
+            'j': self.scroll_down,
+            'k': self.scroll_up
+        }
+
+        if key in key_map:
+            key_map[key]()
+
+        return urwid.ListBox.keypress(self, size, key)
+
+
+
+class Tasky(object):
+
+    palette = [
+        ('proj', '', '', '', 'dark green', ''),
+        ('proj_focus', '', '', '', 'dark gray', 'dark green'),
+        ('body','', '', '', 'dark blue', ''),
+        ('body_focus', '', '', '', 'dark gray', 'dark cyan'),
+        ('head', '', '', '',  'light red', 'black'),
+        ('dim', '', '', '', 'g54', 'black')
+    ]
+
+    def __init__(self):
+
+        self.warrior = TaskWarrior()
+
+        self.filter = ''.join(sys.argv[1:])
+
+        header = urwid.AttrMap(urwid.Text('tasky.α'), 'head')
+        self.walker = urwid.SimpleListWalker([])
+        self.list_box = ScrollingListBox(self.walker)
+        self.view = urwid.Frame(urwid.AttrWrap(self.list_box, 'body'))
+        self.refresh()
+
+        def update_header():
+            filter = ' | ' + self.filter if self.filter else ''
+            header_text = ['tasky.α', ('dim', filter)]
+            self.view.set_header(urwid.AttrMap(urwid.Text(header_text), 'head'))
+
+        update_header()
+
+        loop = urwid.MainLoop(self.view, Tasky.palette, unhandled_input=self.keystroke)
+        urwid.connect_signal(self.walker, 'modified', update_header)
+        loop.screen.set_terminal_properties(colors=256)
+        loop.run()
+
+    def refresh(self):
+        filter = self.filter or ''
+        self.walker[:] = map(TaskWidget, self.warrior.pending_tasks(filter))
+
+    def keystroke(self, input):
+        def exit():
+            raise urwid.ExitMainLoop()
+
+        def undo():
+            self.warrior.undo()
+            self.refresh()
+
+        view_action_map = {
+            'q': exit,
+            'Q': exit,
+            'r': self.refresh,
+            'u': undo,
+            'i': self.new_task,
+            ':': self.command_mode,
+            '!': self.shell_mode,
+            'f': self.change_filter
+        }
+
+        task_action_map = {
+            'enter': (self.edit_task, False),
+            'e': (self.edit_task, False),
+            'n': (self.task_note, False),
+            'c': (self.warrior.complete, True),
+            'd': (self.warrior.delete, True),
+        }
+
+        if input in view_action_map:
+            view_action_map[input]()
+
+        if input in task_action_map:
+            (action, should_refresh) = task_action_map[input]
+            action(self.selected_task())
+            if should_refresh:
+                self.refresh()
+
+    def selected_task(self):
+        return self.list_box.get_focus()[0].task
+
+
+    def task_note(self, task):
+        subprocess.call("tmux split-window 'tasknote %i'" % task['id'], shell=True)
+
+    def present_editor(self, prompt, text, handler):
+        self.foot = LineEditor(prompt, text)
+        self.view.set_footer(self.foot)
+        self.view.set_focus('footer')
+        urwid.connect_signal(self.foot, 'done', handler)
+
+    def command_mode(self):
+        self.present_editor(': ', '', self.command_done)
+
+    def shell_mode(self):
+        self.present_editor('! ', '', self.shell_done)
+
+    def change_filter(self):
+        self.present_editor('task export ', '', self.filter_done)
+
+    def edit_task(self, task):
+        self.edited_task = task
+        self.present_editor(' >> ', task['description'], self.edit_done)
+
+    def new_task(self):
+        self.present_editor(' >> ', '', self.new_done)
+
+
+    def dismiss_editor(action):
+        def wrapped(self, content):
+            self.view.set_focus('body')
+            urwid.disconnect_signal(self, self.foot, 'done', action)
+            if content is not None:
+                action(self, content)
+            self.view.set_footer(None)
+        return wrapped
+
+    @dismiss_editor
+    def edit_done(self, content):
+        self.warrior.mod(self.edited_task, content)
+        self.edited_task = None
+        self.refresh()
+
+    @dismiss_editor
+    def new_done(self, content):
+        filter = self.filter or ''
+        self.warrior.add(content + ' ' + filter)
+        self.refresh()
+
+    @dismiss_editor
+    def command_done(self, content):
+        subprocess.call("task %s" % content)
+        self.refresh()
+
+    @dismiss_editor
+    def shell_done(self, content):
+        subprocess.call("tmux split-window '%s'" % content, shell=True)
+
+    @dismiss_editor
+    def filter_done(self, content):
+        self.filter = content
+        self.refresh()
+
+
+Tasky()
